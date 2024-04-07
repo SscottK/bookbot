1101d714c4e210c5a404f80c4d80c077dfa384be
SscottK <gr8whtninjassk@gmail.com>
2024-04-06 17:45:56 -0700

    string of characters and total count

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+    lowered_text = text.lower()
+    counts = letter_count(lowered_text)
+    print(counts)
 
 
 def get_num_words(text):
@@ -13,6 +16,22 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def letter_count(lowered_text):
+    amount = 0
+    lowers = []
+    
+    for i in range(len(lowered_text)):
+        if not lowered_text[i] in lowers:
+            lowers.append(lowered_text[i])
+            amount += 1
+        else:
+            amount += 1
+    
+        
+        
+    return lowers, amount
+    
 
 
 main()
