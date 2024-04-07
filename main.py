df17760b1ec12c9cc280d464145425d4ec9ab3ea
SscottK <gr8whtninjassk@gmail.com>
2024-04-07 00:26:35 -0700

    Working letter counter

--- main.py
+++ main.py
@@ -7,31 +7,24 @@ def main():
     counts = letter_count(lowered_text)
     print(counts)
 
-
+#this is where we split the book and return the number of words
 def get_num_words(text):
     words = text.split()
     return len(words)
 
-
+#here we are opening the file to be read
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+#here we add the letter to a the dictionary if it doesn't exist and update the count
 def letter_count(lowered_text):
-    amount = 0
-    lowers = []
-    
+    counts_dict = {}    
     for i in range(len(lowered_text)):
-        if not lowered_text[i] in lowers:
-            lowers.append(lowered_text[i])
-            amount += 1
+        if not lowered_text[i] in counts_dict.keys():
+            counts_dict[lowered_text[i]]= 1
         else:
-            amount += 1
-    
-        
-        
-    return lowers, amount
-    
-
+            counts_dict[lowered_text[i]] =  counts_dict[lowered_text[i]] + 1   
+    return counts_dict
 
 main()
