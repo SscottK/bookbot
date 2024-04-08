c44c5867c29794066cfad04a8f2163021e08255b
SscottK <gr8whtninjassk@gmail.com>
2024-04-07 17:19:58 -0700

    Working letter count report

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 def main():
+    print("--- Begin report of books/frankenstein.txt ---")
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+    print()
     lowered_text = text.lower()
     counts = letter_count(lowered_text)
-    print(counts)
+    letter_report(counts)
+    print("--- End report ---")
+   
+    
 
 #this is where we split the book and return the number of words
 def get_num_words(text):
@@ -26,5 +31,19 @@ def letter_count(lowered_text):
         else:
             counts_dict[lowered_text[i]] =  counts_dict[lowered_text[i]] + 1   
     return counts_dict
+
+def letter_report(counts):
+    characters = []
+    for key, value in counts.items():
+        if key.isalpha():
+            characters.append({"letter": key, "count": value})
+
+    def sort_on(dict):
+        return dict["count"]
+    
+
+    characters.sort(reverse=True, key=sort_on)
+    for i in characters:
+        print(f"The {i["letter"]} character was found {i["count"]} times") 
 
 main()
